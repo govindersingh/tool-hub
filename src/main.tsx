38633fda72c7f1c50a909addc3b57349b78da543
Govinder singh <govindersingh05@gmail.com>
2025-10-08 11:13:59 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,23 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App';
 import './index.css';
+import App from './App';
+import LoanCalculator from './pages/LoanCalculator';
+import GSTCalculator from './pages/GSTCalculator';
+import TipCalculator from './pages/TipCalculator';
+import PercentageCalculator from './pages/PercentageCalculator';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Router>
+      <Routes>
+        <Route path="/" element={<App />} />
+        <Route path="/loan-calculator" element={<LoanCalculator />} />
+        <Route path="/gst-calculator" element={<GSTCalculator />} />
+        <Route path="/tip-calculator" element={<TipCalculator />} />
+        <Route path="/percentage-calculator" element={<PercentageCalculator />} />
+      </Routes>
+    </Router>
   </StrictMode>,
 );
