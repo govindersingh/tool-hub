7f31038f67aa993dfeaab75f06716183b438b5b5
Govinder singh <govindersingh05@gmail.com>
2025-10-08 13:08:30 +0530

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -4,9 +4,6 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App';
 import LoanCalculator from './pages/LoanCalculator';
-import GSTCalculator from './pages/GSTCalculator';
-import TipCalculator from './pages/TipCalculator';
-import PercentageCalculator from './pages/PercentageCalculator';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
@@ -14,9 +11,6 @@ createRoot(document.getElementById('root')!).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/loan-calculator" element={<LoanCalculator />} />
-        <Route path="/gst-calculator" element={<GSTCalculator />} />
-        <Route path="/tip-calculator" element={<TipCalculator />} />
-        <Route path="/percentage-calculator" element={<PercentageCalculator />} />
       </Routes>
     </Router>
   </StrictMode>,
